b74c82d0fe8ce68c9b42140df6508885318a9fba
Michel Ortega <coolnerdcool@gmail.com>
2019-10-29 17:33:06 -0600

    [FEAT]: Create the TableViewCell file.

---
+++ onetodo/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  onetodo
+//
+//  Created by Michel Ortega on 10/29/19.
+//  Copyright © 2019 Michel Ortega. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- onetodo/TableViewController.swift
+++ onetodo/TableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class TableViewController: UITableViewController {
 	//	IBOutlets
 	
 
