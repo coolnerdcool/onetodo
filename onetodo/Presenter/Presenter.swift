efb015d114584c7d00a3388e17cd9b7cafdb9d26
Michel Ortega <coolnerdcool@gmail.com>
2019-11-30 18:12:47 -0600

    [FEAT] Implement protocol.

--- onetodo/Presenter/Presenter.swift
+++ onetodo/Presenter/Presenter.swift
@@ -1,14 +1,21 @@
 //
 //  Presenter.swift
 //  onetodo
-//
+//	EX!
 //  Created by Michel Ortega on 10/30/19.
 //  Copyright © 2019 Michel Ortega. All rights reserved.
 //
 
 import Foundation
 
-class Presenter {
+protocol taskProtocol {
+	func createTas()
+	func editTask()
+	func deleteTask()
 	
+}
+
+//	This class handles the information between the view and the logic of the app.
+class Presenter {
 	
 }
