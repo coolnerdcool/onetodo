626c16f4401dcb2605dcc0eeb6940a88498aaa47
Michel Ortega <coolnerdcool@gmail.com>
2020-01-26 22:58:18 -0600

    [FEAT]: Create new class for random DetailTaskLabel.

---
+++ onetodo/Model/DetailTaskLabelModel.swift
@@ -0,0 +1,26 @@
+//
+//  DetailTaskLabelModel.swift
+//  onetodo
+//
+//  Created by Michel Ortega on 1/26/20.
+//  Copyright © 2020 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailTaskLabelModel {
+	
+	//	let task: String
+
+	
+	func randomDetailTaskLabel(_ task: String) {
+		let detailTaskLabels = [
+			"Your current task:\n'\(task)'\n is the only thing that matters now.",
+			"Your"
+		]
+		
+	}
+	
+	
+}
