660bdbb741f56aa5bb36c34f737a29e71d78550e
Michel Ortega <coolnerdcool@gmail.com>
2020-02-12 19:27:29 -0600

    [FEAT]: Implement UIColor extension for generated random color.
    TODO: Implement checkbock in taskView.

---
+++ onetodo/Extensions/UIColor+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Ext.swift
+//  onetodo
+//
+//  Created by Michel Ortega on 2/12/20.
+//  Copyright © 2020 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+	
+	/// Return a random generated UIColor.
+	func random() -> UIColor {
+		
+		let redValue: CGFloat = .random(in: 0...1)
+		let greenValue: CGFloat = .random(in: 0...1)
+		let blueValue: CGFloat = .random(in: 0...1)
+		
+		return UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+	}
+}
