94010236b2296608c83b61deeaad19a51a1345ef
Michel Ortega <coolnerdcool@gmail.com>
2020-02-21 07:22:16 -0600

    [FEAT]: Create TimeSliderHelper and TimeSliderModel classes.
    TODO: Implement both.

---
+++ onetodo/Model/TimeSliderModel.swift
@@ -0,0 +1,15 @@
+//
+//  TimeSliderModel.swift
+//  onetodo
+//
+//  Created by Michel Ortega on 2/21/20.
+//  Copyright © 2020 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimeSliderModel {
+	// TODO: Implement logic for the Time Slider.
+	
+}
