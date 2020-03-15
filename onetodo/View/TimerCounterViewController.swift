8b7aed8ed07c98744dad65964bb89bde80060871
Michel Ortega <coolnerdcool@gmail.com>
2020-03-15 13:19:22 -0600

    [FEAT]: Create new Storyboard and class for TimerCounter

---
+++ onetodo/View/TimerCounterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TimerCounterViewController.swift
+//  onetodo
+//
+//  Created by Michel Ortega on 3/15/20.
+//  Copyright © 2020 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimerCounterViewController: UIViewController {
+	//TODO:	-	THIS VIEW IS SHOWs AFTER USER TOUCHES BUTTON TO START TIMER.
+}
