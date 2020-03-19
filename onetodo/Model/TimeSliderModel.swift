86e3f57debc651759fe69312a3ec516889af12bb
Michel Ortega <coolnerdcool@gmail.com>
2020-03-19 06:19:11 -0600

    [FIX]

--- onetodo/Model/TimeSliderModel.swift
+++ onetodo/Model/TimeSliderModel.swift
@@ -14,6 +14,7 @@ class TimeSliderModel: UIViewController, RangeUISliderDelegate {
 	//TODO:	-	IMPLEMENT THE LOGIC FOR THE TIME SLIDER.
 	//MARK:	-	PROPERTIES.
 	
+	
 	//MARK:	-	METHODS.
 	
 	/// Make value needed to estimate time for TimeSlider.
